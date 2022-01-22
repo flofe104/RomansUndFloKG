f3b4ee0a0f5d4466671e2d2d0e05399c2fccc76a
Florian <florian.kohrs@web.de>
2022-01-22 14:00:37 +0100

    Remove problems and buggs in dungeon test

--- HellKnight/Assets/Testing/Tests/DungeonTest.cs
+++ HellKnight/Assets/Testing/Tests/DungeonTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -6,10 +7,17 @@ using UnityEngine.TestTools;
 
 public class DungeonTest : Dungeon
 {
-    
+
+    protected void InitializeEnemyList()
+    {
+        UnityEngine.Object o = Resources.Load("TestEnemy");
+        possibleEnemies = new List<EnemySciptableObject>() { o as EnemySciptableObject };
+    }
+
     [Test]
     public void TestNumberOfRooms()
     {
+        InitializeEnemyList();
         int seed = 0;
         InitializeDungeon(seed);
 
@@ -19,38 +27,56 @@ public class DungeonTest : Dungeon
     [Test]
     public void TestRandomness()
     {
-        MeshFilter[] filters = new MeshFilter[5];
-        MeshCollider[] meshColliders = new MeshCollider[5];
+        InitializeEnemyList();
+        Vector3[][] vertices = new Vector3[5][];
+        Vector3[][] colliderVerts = new Vector3[5][];
         for (int i = 0; i < 5; ++i)
         {
+            DisplayDungeonAction = delegate 
+            { 
+                vertices[i] = base.vertices.ToArray(); 
+                colliderVerts[i] = colliderVertices.ToArray(); 
+            };
             InitializeDungeon(i);
-            filters[i] = filter;
-            meshColliders[i] = meshCollider;
         }
 
         for (int i = 0; i < 5; ++i)
         {
-            for(int j = 0; j < 5; ++j)
+            for(int j = i + 1; j < 5; ++j)
             {
-                Assert.AreNotEqual(filters[i], filters[j]);
-                Assert.AreNotEqual(meshColliders[i], meshColliders[j]);
+                Assert.AreNotEqual(vertices[i], vertices[j]);
+                Assert.AreNotEqual(colliderVerts[i], colliderVerts[j]);
             }
         }
     }
 
+    protected override void DisplayDungeon()
+    {
+        DisplayDungeonAction?.Invoke();
+    }
+
+    protected Action DisplayDungeonAction;
+
     [Test]
     public void TestNormals()
     {
-        var cameraPosition = Camera.main.transform.forward;
-
-        for(int i=0; i<triangles.Count; i+=3)
+        InitializeEnemyList();
+        List<int> triangles = null;
+        List<Vector3> vertices = null;
+        DisplayDungeonAction = delegate 
+        { 
+            triangles = base.triangles; 
+            vertices = base.vertices; 
+        };
+        InitializeDungeon(0);
+        for (int i=0; i<triangles.Count; i+=3)
         {
             var v0 = vertices[triangles[i]];
             var v1 = vertices[triangles[i+1]];
             var v2 = vertices[triangles[i+2]];
             var normal = (Vector3.Cross(v1 - v0, v2 - v0)).normalized;
 
-            Assert.LessOrEqual(Vector3.Angle(normal, cameraPosition), 180);
+            Assert.AreEqual(Vector3.Angle(normal, -Vector3.forward), 0);
         }
     }
 }
