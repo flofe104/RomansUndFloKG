523a5cfcbf26036a865e086116fb3f1255ad5c31
Xentis98 <36013464+Xentis98@users.noreply.github.com>
2022-01-24 19:04:37 +0100

    add enemy hover

--- HellKnight/Assets/Scripts/Combat/EnemyCombat/EnemyCombat.cs
+++ HellKnight/Assets/Scripts/Combat/EnemyCombat/EnemyCombat.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class EnemyCombat : MonoBehaviour
 {
     public float attackCooldown = 2;
+    public float hoverSpeed = 0.8f;
+    public float hoverDistance = 0.8f;
+    public AnimationCurve hoverPatternX;
+    public AnimationCurve hoverPatternY;
     public GameObject projectilePrefab;
 
-    private Movement enemyMovement;
     private float timeSinceAttack = 0;
+    private float t = 0;
     private GameObject player;
+    private Vector3 center;
 
     private void Start()
     {
         player = GameObject.Find("Player");
+        center = transform.position;
     }
     void Update()
     {
         Attack();
+        Hover();
     }
 
     private void Attack()
@@ -31,4 +38,12 @@ public class EnemyCombat : MonoBehaviour
         }
     }
 
+    private void Hover()
+    {
+        var pos = transform.position;
+        t += 1 / hoverSpeed * Time.deltaTime;
+        var x = hoverPatternX.Evaluate(t);
+        var y = hoverPatternY.Evaluate(t);
+        transform.position = center + hoverDistance * new Vector3(x, y);
+    }
 }
