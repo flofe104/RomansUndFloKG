42753be158f47b156fa2422b26ec4a776ddf9ea1
Florian <florian.kohrs@web.de>
2022-03-29 11:59:13 +0200

    Spawn melee enemy on ground and increase arrow speed

--- HellKnight/Assets/Scripts/Enemies/EnemySciptableObject.cs
+++ HellKnight/Assets/Scripts/Enemies/EnemySciptableObject.cs
@@ -13,24 +13,27 @@ public class EnemySciptableObject : ScriptableObject, ISpawnableEnemy
     protected GameObject prefab;
     public GameObject Prefab { get { return prefab; } }
 
+    [SerializeField]
+    protected bool spawnOnGround = false;
+    
+    public bool SpawnOnGround => spawnOnGround;
 
 
     [SerializeField]
     protected int damage;
     public int Damage { get { return damage; } }
 
-    public int ChallengeRating
-    {
-        get
-        {
-            return health / 10 + 2 * damage;
-        }
-    }
-
+    public int ChallengeRating => health / 10 + 2 * damage;
+        
     public GameObject Spawn(Vector3 position, Transform parent)
     {
+        if(spawnOnGround)
+        {
+            position.y = 1.14f;
+        }
         GameObject gameObject = Instantiate(prefab, position, Prefab.transform.rotation);
         gameObject.transform.SetParent(parent, false);
+        gameObject.transform.localPosition = position;
         BaseHealth health = gameObject.GetComponent<BaseHealth>();
         health.ResetWithMaxHealth(Health);
         return gameObject;
